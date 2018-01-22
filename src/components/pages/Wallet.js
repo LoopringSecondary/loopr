282a916278facb317b02cccd8e666aa0111b5c9c
houzongbao <houzongbao@126.com>
2018-01-22 16:26:47 +0800

    update routes

--- src/components/pages/Wallet.js
+++ src/components/pages/Wallet.js
@@ -34,6 +34,5 @@ export default function Home(props){
         </Tabs.TabPane>
       </Tabs>
     </div>
-    
   )
 }
--- src/components/routes.js
+++ src/components/routes.js
@@ -3,13 +3,17 @@ import { Route, Switch } from 'dva/router';
 import Pages from './pages';
 import ordersRoutes from './orders/routes';
 import ringsRoutes from './rings/routes';
-
 export default (
-		<Switch>
-			<Route path="/" exact component={Pages.Wallet} />
-			<Route path="/wallet" exact component={Pages.Wallet} />
-			<Route path="/market" exact component={Pages.Market} />
-			{ordersRoutes}
+	<div>
+		<div>
+			<Switch>
+				<Route path="/" exact component={Pages.Wallet} />
+				<Route path="/wallet" exact component={Pages.Wallet} />
+				<Route path="/market" exact component={Pages.Market} />
+			</Switch>
 			{ringsRoutes}
-		</Switch>
+			{ordersRoutes}
+		</div>
+	</div>
+		
 )
