d843f6c5e8ad222f87c57331c9d700cbe45f83b4
houzongbao <houzongbao@126.com>
2018-01-22 12:35:52 +0800

    update transferPreview

--- src/components/tokens/components/TransferPreview.js
+++ src/components/tokens/components/TransferPreview.js
@@ -2,9 +2,32 @@ import React from 'react';
 import { Avatar,Icon,Button } from 'antd';
 
 let Preview = ({
-  form,
   modal,
   }) => {
+
+  const MetaItem = (props)=>{
+    const {label,value} = props
+    return (
+      <div className="row pt10 pb10 zb-b-b">
+        <div className="col">
+          <div className="fs14 color-grey-600">{label}</div>
+        </div>
+        <div className="col-auto">
+          <div className="fs14 color-grey-900">{value}</div>
+        </div>
+      </div>
+    )
+  }
+  const ArrowDivider = (
+      <div className="row no-gutters align-items-center">
+        <div className="col">
+          <hr className="w-100 bg-grey-900"/>
+        </div>
+        <div className="col-auto">
+          <Icon type="right" className="color-grey-900" style={{marginLeft:'-9px'}}></Icon>
+        </div>
+      </div>
+  )
   return (
       <div>
         <div className="row flex-nowrap zb-b-b pb30">
@@ -18,16 +41,7 @@ let Preview = ({
             <div className="text-center">
               <Avatar size="" className="bg-white border-grey-900" src="https://loopring.io/images/favicon.ico"></Avatar>
               <div className="fs12 color-grey-500">LRC</div>
-              <div className="fs12  ">
-                <div className="row no-gutters align-items-center">
-                  <div className="col">
-                    <hr className="w-100 bg-grey-900"/>
-                  </div>
-                  <div className="col-auto">
-                    <Icon type="right" className="color-grey-900" style={{marginLeft:'-9px'}}></Icon>
-                  </div>
-                </div>
-              </div>
+              {ArrowDivider}
               <div className="fs14 color-grey-900">-2000LRC($4000)</div>
             </div>
           </div>
@@ -38,46 +52,11 @@ let Preview = ({
             </div>
           </div>
         </div>
-        <div className="row pt10 pb10 zb-b-b">
-          <div className="col">
-            <div className="fs14 color-grey-600">To</div>
-          </div>
-          <div className="col-auto">
-            <div className="fs14 color-grey-900">0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00</div>
-          </div>
-        </div>
-        <div className="row pt10 pb10 zb-b-b">
-          <div className="col">
-            <div className="fs14 color-grey-600">From</div>
-          </div>
-          <div className="col-auto">
-            <div className="fs14 color-grey-900">0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00</div>
-          </div>
-        </div>
-        <div className="row pt10 pb10 zb-b-b">
-          <div className="col">
-            <div className="fs14 color-grey-600">GasLimit</div>
-          </div>
-          <div className="col-auto">
-            <div className="fs14 color-grey-900">2100</div>
-          </div>
-        </div>
-        <div className="row pt10 pb10 zb-b-b">
-          <div className="col">
-            <div className="fs14 color-grey-600">GasPrice</div>
-          </div>
-          <div className="col-auto">
-            <div className="fs14 color-grey-900">21 Gwei ( 0.000012 LRC )</div>
-          </div>
-        </div>
-        <div className="row pt10 pb10 zb-b-b">
-          <div className="col">
-            <div className="fs14 color-grey-600">Max Tx Fee</div>
-          </div>
-          <div className="col-auto">
-            <div className="fs14 color-grey-900">12 LRC ( 420000 Gwei )</div>
-          </div>
-        </div>
+        <MetaItem label="To" value="0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00" />
+        <MetaItem label="From" value="0xebA7136A36DA0F5e16c6bDBC739c716Bb5B65a00" />
+        <MetaItem label="GasLimit" value="2100" />
+        <MetaItem label="GasPrice" value="21 Gwei ( 0.000012 LRC )" />
+        <MetaItem label="Max Tx Fee" value="12 LRC ( 420000 Gwei )" />
         <div className="row pt40">
           <div className="col pl0">
             <Button className="d-block w-100" type="" size="large">No, Cancel It</Button>
