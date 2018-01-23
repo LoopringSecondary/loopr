06c21f9fb108b67d0b77c296e341de815de36f91
houzongbao <houzongbao@126.com>
2018-01-23 11:18:42 +0800

    update locales module

--- src/modules/locales/container.js
+++ src/modules/locales/container.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import enUS from 'antd/lib/locale-provider/en_US'; 
 import {IntlProvider,addLocaleData,injectIntl} from 'react-intl';
 import en from 'react-intl/locale-data/en';  
 import zh from 'react-intl/locale-data/zh';  
-addLocaleData([...en, ...zh]);  
+addLocaleData([...en, ...zh]); 
 
 const Locales = ({ history,locales={},children})=>{
 		const appProps = {
@@ -12,7 +15,9 @@ const Locales = ({ history,locales={},children})=>{
 		}
 	  return (
 	  	<IntlProvider {...appProps}>
-	  		{children}
+	  		<LocaleProvider locale={enUS}>
+	  			{children}
+	  		</LocaleProvider>
 	  	</IntlProvider>
 	  )
 }
--- src/router.js
+++ src/router.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
 import Layout from './layout/Layout';
-import { LocaleProvider } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
-import enUS from 'antd/lib/locale-provider/en_US';
 import pageRoutes from './components/routes';
-import Locales from './locales'
+import Locales from './modules/locales/container'
 function RouterConfig({ history }) {
   return (
     <Locales>
-        <LocaleProvider locale={enUS}>
-            <Router history={history}>
-    			<Layout>
-    			    {pageRoutes}
-    			</Layout>
-            </Router>
-        </LocaleProvider>
+        <Router history={history}>
+    		<Layout>
+    		    {pageRoutes}
+    		</Layout>
+        </Router>
     </Locales>
     
   );
