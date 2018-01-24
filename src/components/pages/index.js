59fb862d31632dacd3645cde3de077ef6b724f6d
lyd <lyddream@126.com>
2018-01-24 19:10:15 +0800

    add portfolio

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,8 +1,10 @@
 
 import Wallet from './Wallet'
 import Market from './Market'
+import Portfolio from './Portfolio'
 
 export default {
   Wallet,
   Market,
+  Portfolio,
 }
--- src/modules/locales/localesData/en.js
+++ src/modules/locales/localesData/en.js
@@ -2,5 +2,6 @@ export default {
 	"navbar.market":'Market',
 	"navbar.wallet":'Wallet',
 	"navbar.setting":'Setting',
+  "navbar.portfolio":"Portfolio",
 	"app.title":'Loopring Wallet',
 }
