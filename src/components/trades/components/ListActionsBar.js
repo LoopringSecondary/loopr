81197f6668e8267b4ce841b99e23fe12d02275f3
houzongbao <houzongbao@126.com>
2018-01-29 12:39:28 +0800

    update order list ui

--- src/components/trades/components/ListActionsBar.js
+++ src/components/trades/components/ListActionsBar.js
@@ -5,7 +5,7 @@ import {Button} from 'antd'
 function ListActionsBar({actions={},LIST={}}){
   return (
     <div>
-    	<div className="row">
+    	<div className="row ml0 mr0">
     		<div className="col-auto">
     				<ListFiltersForm actions={actions} LIST={LIST} />
     		</div>
