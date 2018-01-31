bf57fae9eb934cd6e6fc3083d00cf552668c0337
houzongbao <houzongbao@126.com>
2018-01-31 23:58:03 +0800

    update generate wallet form ux

--- src/components/wallet/components/GenerateWallet.js
+++ src/components/wallet/components/GenerateWallet.js
@@ -10,6 +10,8 @@ export default class GenerateWallet extends React.Component {
     super(props)
     this.state = {
       visible:false,
+      strength:'',
+      disabled:true,
       value:''
     }
   }
@@ -18,12 +20,30 @@ export default class GenerateWallet extends React.Component {
       visible:!this.state.visible
     })
   }
-  passwordChange(value){
-    console.log('value',value)
+  passwordChange(e){
+    const value = e.target.value
+    const strength = this.getStrength(value)
+    this.setState({
+      value,
+      strength
+    })
   }
-
+  getStrength(value){
+    if(value.length<=6){
+      return 'weak'
+    }
+    if(6 < value.length <= 10){
+      return 'average'
+    }
+    if(value.length > 10){
+      console.log('value.length',value.length)
+      return 'strong'
+    }
+  }
   render() {
     const {modals} = this.props
+    const {visible,strength,disabled,value} = this.state
+
     const handelSubmit = ()=>{
       modals.hideModal({id:'wallet/generate'})
       modals.showModal({id:'wallet/backup'})
@@ -32,47 +52,50 @@ export default class GenerateWallet extends React.Component {
       modals.hideModal({id:'wallet/generate'})
       modals.showModal({id:'wallet/unlock'})
     }
-    const footer = (
-      <div className="fs14 color-grey-900 text-center pt10 pb10">
-        Already have a wallet ? <a className="color-blue-600 ml5" onClick={gotoUnlock}>Click to unlock</a> !
-      </div>
-    )
-    const passwordVisible = (
+    const visibleIcon = (
         <div className="fs14 pl5 pr5">
-          {
-            this.state.visible && 
+          { visible && 
             <i className="fa fa-eye" onClick={this.togglePassword.bind(this)}></i>
           }
-          {
-            !this.state.visible && 
+          { !visible && 
             <i className="fa fa-eye-slash" onClick={this.togglePassword.bind(this)}></i>
           }
         </div>
     )
-    const progressFormat = (percent)=>{
-      return (
-        'Strong'
-      )
-    }
     return (
       <Card title='Generate Wallet'>
         <Input 
-          type={this.state.visible ? 'text' : 'password'} 
+          type={visible ? 'text' : 'password'} 
+          value={value}
           size="large" 
           placeholder="Set a strong password"
-          addonAfter={passwordVisible}
+          addonAfter={visibleIcon}
           onChange={this.passwordChange.bind(this)}
         />
-        <div className="row pt10 pb10">
-          <div className="col-auto pr0">
-            <span className="fs12 color-grey-900">Password Strength</span>
+        {
+          value &&
+          <div className="row pt10 pb10">
+            <div className="col-auto pr0">
+              <span className="fs12 color-grey-900">Password Strength</span>
+            </div>
+            <div className="col-5">
+              { strength == 'weak' &&
+                <Progress className="d-inline-block" percent={30} strokeWidth={4}  status="exception" format={()=>strength}/>
+              }
+              { strength == 'average' &&
+                <Progress className="d-inline-block" percent={60} strokeWidth={4}  status="active" format={()=>strength}/>
+              }
+              { strength == 'strong' &&
+                <Progress className="d-inline-block" percent={90} strokeWidth={4}  status="success" format={()=>strength}/>
+              }
+            </div>
           </div>
-          <div className="col-5">
-            <Progress className="d-inline-block" percent={80} strokeWidth={4}  status="success" showInfo={false}  format={progressFormat}/>
-          </div>
+        }
+        <div className="mb25"></div>
+        <Button disabled={disabled} onClick={handelSubmit} className="w-100 d-block mt15" type="primary" size="large" >Generate Now</Button>
+        <div className="fs14 color-grey-900 text-center pt10 pb10">
+          Already have a wallet ? <a className="color-blue-600 ml5" onClick={gotoUnlock}>Click to unlock</a> !
         </div>
-        <Button onClick={handelSubmit} className="w-100 d-block mt15" type="primary" size="large" >Generate Now</Button>
-        {footer}
       </Card>
     );
   }
