f55d4fffe204148848a15662dc892c047876caf3
houzongbao <houzongbao@126.com>
2018-02-03 00:23:44 +0800

    complete page wallet tabs routes

--- src/components/pages/Wallet.js
+++ src/components/pages/Wallet.js
@@ -10,13 +10,11 @@ import Ring from '../rings/pages'
 import Layout from '../../layout/Layout'
 
 export default function Home(props){
-  const { children } = props
-  const { dispatch, match } = props;
-  const TabTilte = (props)=> <div className="fs18 pb5 pt5">{props.children}</div>
+  const { children,match,location } = props
+  
   const handleTabChange = (key) => {
     switch (key) {
       case 'assets':
-        // dispatch(routerRedux.push(`${match.url}/articles`));
         window.routeActions.gotoPath(`${match.url}/assets`)
         break;
       case 'orders':
@@ -29,70 +27,41 @@ export default function Home(props){
         break;
     }
   }
+  const TabTilte = (props)=> <div className="fs18 pb5 pt5">{props.children}</div>
   return (
     <Layout {...props}>
       <div className="container">
-        <div className=" ">
-
-          <Tabs onChange={handleTabChange} defaultActiveKey="assets" animated={false} tabBarStyle={{paddingLeft:'50px',marginBottom:'0px'}}>
-            <Tabs.TabPane tab={<div className="fs18 pb5 pt5">Assets</div>} key="assets" />
-            <Tabs.TabPane tab={<div className="fs18 pb5 pt5">Orders</div>} key="orders" />
-            <Tabs.TabPane tab={<div className="fs18 pb5 pt5">Trades</div>} key="trades" />
+          <Tabs className="rs nobar" onChange={handleTabChange} activeKey={location.pathname.replace(`${match.path}/`, '')} animated={false}>
+            <Tabs.TabPane tab={<div className="fs18 pl0 pr20 pt30 pb20 "><FormattedMessage id="page.wallet.assets"/></div>} key="assets" />
+            <Tabs.TabPane tab={<div className="fs18 pl20 pr20 pt30 pb20 "><FormattedMessage id="page.wallet.orders"/></div>} key="orders" />
+            <Tabs.TabPane tab={<div className="fs18 pl20 pr20 pt30 pb20 "><FormattedMessage id="page.wallet.trades"/></div>} key="trades" />
           </Tabs>
-            <Switch>
-              <Route path={`${match.url}/assets`} exact render={()=>
-                <div className="row no-gutters bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
-                  <div className="col-4 zb-b-r">
-                   <Token.ListSidebar />
-                  </div>
-                  <div className="col-8">
-                   <Transaction.ListStand />
-                  </div>
+          <Switch>
+            <Route path={`${match.url}/assets`} exact render={()=>
+              <div className="row no-gutters bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
+                <div className="col-4 zb-b-r">
+                 <Token.ListSidebar />
                 </div>
-              }
-              />
-              <Route path={`${match.url}/orders`} exact render={()=>
-                <div className="pt15 pb15 bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
-                  <Order.List />
+                <div className="col-8">
+                 <Transaction.ListStand />
                 </div>
-              }
-              />
-              <Route path={`${match.url}/trades`}  render={()=>
-                <div className="pt15 pb15 bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
-                  <Trade.List />
-                </div>
-              } 
-              />
-              <Redirect path={`${match.url}/`} to={`${match.url}/assets`} />
-            </Switch>
-          {
-            false &&
-            <Tabs className="rs nobar" defaultActiveKey="assets" animated={false}>
-              <Tabs.TabPane tab={<div className="fs18 pl0 pr20 pt30 pb20"><FormattedMessage id="page.wallet.assets"/></div>} key="assets">
-               <div className="row no-gutters bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
-                 <div className="col-4 zb-b-r">
-                  <Token.ListSidebar />
-                 </div>
-                 <div className="col-8">
-                  <Transaction.ListStand />
-                 </div>
-               </div>
-              </Tabs.TabPane>
-              <Tabs.TabPane tab={<div className="fs18 pl20 pr20 pt30 pb20"><FormattedMessage id="page.wallet.orders"/></div>} key="orders">
-                <div className="pt15 pb15 bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
-                  <Order.List />
-                </div>
-              </Tabs.TabPane>
-              <Tabs.TabPane tab={<div className="fs18 pl20 pr20 pt30 pb20"><FormattedMessage id="page.wallet.trades"/></div>} key="trades">
-                <div className="pt15 pb15 bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
-                  <Trade.List />
-                </div>
-              </Tabs.TabPane>
-            </Tabs>
-
-          }
-          
-        </div>
+              </div>
+            }
+            />
+            <Route path={`${match.url}/orders`} exact render={()=>
+              <div className="pt15 pb15 bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
+                <Order.List />
+              </div>
+            }
+            />
+            <Route path={`${match.url}/trades`}  render={()=>
+              <div className="pt15 pb15 bg-white" style={{borderRadius:'6px',border:'1px solid #dadada'}}>
+                <Trade.List />
+              </div>
+            } 
+            />
+            <Redirect path={`${match.url}/`} to={`${match.url}/assets`} />
+          </Switch>
       </div>
     </Layout>
     
