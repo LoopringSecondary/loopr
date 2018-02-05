18c4730d43fba96d953e8d3ed4b7633cc0f94387
houzongbao <houzongbao@126.com>
2018-02-05 18:50:03 +0800

    add module tickers

---
+++ src/modules/tickers/apis.js
@@ -0,0 +1,25 @@
+import {getOrders} from 'Loopring/relay/market'
+
+
+export async function fetchList(payload){
+    let {page,filters,sort} = payload
+    let filter = {}
+    if(filters){
+      filter.ringHash = filters.ringHash
+    }
+    if(page){
+      filter.pageIndex = page.current
+      filter.pageSize = page.size
+    }
+    filter.contractVersion = 'v1.0'
+    return getOrders(filter).then(res=>{
+      return {
+        items:res.result.data,
+        page:{
+          current:res.result.pageIndex,
+          size:res.result.pageSize,
+          total:res.result.total,
+        }
+      }
+    })
+}
