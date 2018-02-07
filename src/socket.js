bd411106b17e22e9935cb3fa19890251856886f6
houzongbao <houzongbao@126.com>
2018-02-07 12:25:34 +0800

    add socket test

---
+++ src/socket.js
@@ -0,0 +1,8 @@
+import io from 'socket.io-client';
+
+// let host = "https://relay1.loopring.io/ws"
+// let socket = io.connect(host)
+// socket.on('connect', function(data){
+//   console.log("connected with server")
+// })
+
