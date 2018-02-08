ce72f61d3485237e7868fb72cf1c4bf88567eb6c
keith Du <keithdu@keithdeMacBook-Pro.local>
2018-02-08 17:17:19 +0800

    formater

--- src/components/tokens/components/TransferPreview.js
+++ src/components/tokens/components/TransferPreview.js
@@ -9,24 +9,22 @@ let Preview = ({
   const {rawTx,extraData} = modal
   //TODO mock
   const privateKey = "93d2d40c13f4d4ca422c154dac7db78f8b0964ad8aa9047c9eb5dfa750357c4e"
-  const handelSubmit = async ()=>{
+  const handelSubmit = ()=>{
     let tx = new Transaction(rawTx)
     tx.setNonce(extraData.from)
     modal.showLoading({id:'token/transfer/preview'})
     let result = {...rawTx, extraData}
-    try {
-      await tx.send(privateKey).then(res=>{
-        if(res.error) {
-          result = {...result, error:res.error.message}
-        }
-        modal.hideModal({id:'token/transfer/preview'})
-        modal.showModal({id:'token/transfer/result', result})
-      })
-    } catch (e) {
+    tx.send(privateKey).then(res=>{
+      if(res.error) {
+        result = {...result, error:res.error.message}
+      }
+      modal.hideModal({id:'token/transfer/preview'})
+      modal.showModal({id:'token/transfer/result', result})
+    }).catch(e=>{
       result = {...result, error:e.message}
       modal.hideModal({id:'token/transfer/preview'})
       modal.showModal({id:'token/transfer/result', result})
-    }
+    })
   }
 
   const handelCancel = ()=>{
