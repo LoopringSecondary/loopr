da4225f05eb26d0a58ff0d11f07c9aa170813f9c
houzongbao <houzongbao@126.com>
2018-02-27 18:38:21 +0800

    add socket connect callback

--- src/modules/socket/Provider.js
+++ src/modules/socket/Provider.js
@@ -2,14 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import io from 'socket.io-client'
 
+
+
 class SocketProvider extends React.Component {
   getChildContext() {
     return { socket: this.socket }
   }
+  async connect(url){
+    const socket = io(url)
+    return new Promise((resolve)=>{
+      socket.on('connect',()=>{
+        console.log('socket connect success!')
+        resolve(socket)
+      })
+    })
+  }
   constructor(props, context) {
     super(props, context)
     this.socket = io.connect(props.url)
-    console.log('this.socket',this.socket)
+    this.socket.on('connect', (data) => {
+      console.log('socket connected success!',this.socket)
+    })
+    this.socket.on('disconnect', (data) => {
+      console.log('socket disconnect')
+    });
+    this.socket.on('error', (err) => {
+      console.log('socket error',err)
+    });
   }
   render() {
     return React.Children.only(this.props.children)
--- src/modules/socket/ProviderContainer.js
+++ src/modules/socket/ProviderContainer.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import SocketProvider from './Provider';
 
+
 const SocketProviderContainer = (props)=>{
   const { settings } = props
   const host = settings.relay.selected || 'https://relay1.loopring.io'
-  const url = `${host}/ws`
+  const url = `http://10.137.107.92:8087`
   console.log('url',url)
   return (
       <SocketProvider url={url}>
