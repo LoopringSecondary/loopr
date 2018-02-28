ec757e43da35b663511375adb85b30f414509e5e
houzongbao <houzongbao@126.com>
2018-02-28 21:46:37 +0800

    enhancement backup ui

--- src/components/wallet/components/ExportKeystore.js
+++ src/components/wallet/components/ExportKeystore.js
@@ -41,41 +41,43 @@ class ExportKeystore extends React.Component {
     )
     return (
       <Card title="Export Keystore">
-        {tip}
+
         <div className="pt15">
-          <div className="fs14 color-grey-800 mb10">Password</div>
           <Input
             type='password'
+            placeholder="Enter Wallet password"
             size="large"
           />
           <Button disabled size="large" className="d-block w-100 mt25" >
             Get Keystore
           </Button>
-          <Tabs defaultActiveKey="file" tabPosition="" animated={true} style={{marginTop:'-10px'}}>
-            <Tabs.TabPane tab={<div style={{marginLeft:'0px'}} className="fs16 text-center mb5">File</div>} key="keystore">
-            </Tabs.TabPane>
-            <Tabs.TabPane tab={<div style={{marginLeft:'0px'}} className="fs16 text-center mb5">Text</div>} key="mnemonic">
 
+          <Tabs defaultActiveKey="file" tabPosition="" animated={true} className="layout-center layout-col mt15">
+            <Tabs.TabPane tab={<div className="fs16 text-center mb5">File Keystore</div>} key="file">
+              {tip}
+              <a href={''}
+                 download={''}
+                 className="ant-btn ant-btn-primary ant-btn-lg d-flex justify-content-center w-100 mt15 align-items-center">
+                 Download Keystore File
+              </a>
             </Tabs.TabPane>
-            <Tabs.TabPane tab={<div style={{marginLeft:'0px'}} className="fs16 text-center mb5">Qrcode</div>} key="privatekey">
+            <Tabs.TabPane tab={<div className="fs16 text-center mb5">Text Keystore</div>} key="Text">
+              {tip}
+              <Input.TextArea
+                value=""
+                size="large"
+                rows={4}
+                auto={true}
+              />
+              <Button size="large" type="primary" className="d-block w-100 mt25" >
+                Copy Keystore
+              </Button>
+            </Tabs.TabPane>
+            <Tabs.TabPane tab={<div className="fs16 text-center mb5">Qrcode Keystore</div>} key="qrcode">
+              {tip}
+              QRCode
             </Tabs.TabPane>
           </Tabs>
-
-          {
-            false &&
-            <a href={''}
-               download={''}
-               className="ant-btn ant-btn-disabled ant-btn-primary ant-btn-lg d-flex justify-content-center w-100 mt25 align-items-center">
-               Download Keystore File
-            </a>
-          }
-          {
-            true &&
-            <Button disabled size="large" className="d-block w-100 mt25" >
-              Download Keystore File
-            </Button>
-          }
-
         </div>
 
       </Card>
