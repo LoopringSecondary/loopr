0f533d2f6d9a4eb25e0fab72d4e6890ca3e13149
keith Du <keithdu@keithdeMacBook-Pro.local>
2018-03-14 15:01:36 +0800

    error message'

--- src/components/wallet/components/UnlockByMetaMask.js
+++ src/components/wallet/components/UnlockByMetaMask.js
@@ -59,7 +59,7 @@ let UnlockByMetaMask = ({
      })
     } else {
       let content = 'Your may need to install MetaMask extension to your browser first'
-      if(!window.web3.eth.accounts[0]) { // locked
+      if(window.web3 && !window.web3.eth.accounts[0]) { // locked
         content = 'Failed to connect with MetaMask, please unlock and use'
       }
       Modal.error({
