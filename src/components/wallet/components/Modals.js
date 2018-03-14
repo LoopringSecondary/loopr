af373d8d5759dbc3430f0d108f1db51f671b8e23
houzongbao <houzongbao@126.com>
2018-03-14 14:05:48 +0800

    update modal container: add width props

--- src/components/wallet/components/Modals.js
+++ src/components/wallet/components/Modals.js
@@ -9,7 +9,7 @@ import AccountContainer from '../../../modules/account/container'
 function Modals(props){
   return (
     <div>
-			<ModalContainer id="wallet/unlock" >
+			<ModalContainer id="wallet/unlock" width="70%" >
 				<AccountContainer>
 			  	<UnlockWallet />
 			  </AccountContainer>
