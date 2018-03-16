1357cf9f4173340016a9835f042adb5ef1acab97
houzongbao <houzongbao@126.com>
2018-03-16 19:17:49 +0800

    fix bug transation value

--- src/modules/socket/modules/TransactionsContainer.js
+++ src/modules/socket/modules/TransactionsContainer.js
@@ -15,10 +15,9 @@ class TransactionsSocketContainer extends React.Component {
       console.log('socket connection has not been established')
       return false
     }
-    const currency = window.STORAGE.settings.getCurrency() || 'CNY' // TODO
-    const data = {currency}
+    const owner = window.WALLET && window.WALLET.getAddress()
     const query = {
-      owner:'0x750aD4351bB728ceC7d639A9511F9D6488f1E259',
+      owner,
       symbol:'LRC',
       pageIndex:'1',
       pageSize:'20',
