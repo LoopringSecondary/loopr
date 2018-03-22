1c5a768ff800e398aa514cffa8dab33a1fbce694
houzongbao <houzongbao@126.com>
2018-03-22 13:35:01 +0800

    fix icons bug & update icons

--- src/components/common/CoinIcon.js
+++ src/components/common/CoinIcon.js
@@ -1,22 +1,15 @@
 import React, { PropTypes } from 'react';
-import ReactSVG from 'react-svg';
 import colors from '../../common/config/tokens_colors.json'
-// path="//47.100.16.217:8888/icons/LRC.svg"
-// ../../assets/icons/
-// const path = `./${symbol}.svg`
-// const path = `../assets/icons/${symbol}.svg`
 const CoinIcon = (props)=>{
-  let {symbol='LRC',size='50',fill='',color,className="",style={} } = props
+  let {symbol='LRC',size='50',color='',className="",style={} } = props
   symbol = symbol.toUpperCase()
-  // const path = `../../../../release/icons/${symbol}.svg`
-  const path = `../../assets/icons/${symbol}.svg`
   if(!color){
     color = 'color-'+ colors[symbol] || ''
+  }else{
+    color = 'color-'+ color
   }
   return (
     <i className={`icon icon-loopring icon-loopring-${symbol} fs${size} ${color} ${className}`} ></i>
   )
-
 }
-
 export default CoinIcon
