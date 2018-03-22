c404dabb77247ccde67be70c7d8714e0f972073f
lyd <lyddream@126.com>
2018-03-22 12:35:36 +0800

    complete backup wallet locales

--- src/components/wallet/components/BackupKeystore.js
+++ src/components/wallet/components/BackupKeystore.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Form} from 'antd';
 import icon from '../../../assets/images/icon-backup-wallet.png'
 import {download} from "Loopring/ethereum/account"
+import intl from 'react-intl-universal';
 
 class BackupKeystore extends React.Component {
 
@@ -21,20 +22,19 @@ class BackupKeystore extends React.Component {
       <div>
         <div className="text-center">
           <img src={icon} className="mt25 mb25" style={{width: '100px'}}/>
-          <div className="fs20 color-grey-900 mb5">Don't Lose It!</div>
-          <div className="fs14 color-grey-600 mb15">It cannot be recovered if you lose it.</div>
-          <div className="fs20 color-grey-900 mb5">Do not share it!!</div>
-          <div className="fs14 color-grey-600 mb15">Your funds will be stolen if you use this file on a
-            malicious/phishing site.
+          <div className="fs20 color-grey-900 mb5">{intl.get('wallet.backup.not_lose')}!</div>
+          <div className="fs14 color-grey-600 mb15">{intl.get('wallet.backup.not_recover')}.</div>
+          <div className="fs20 color-grey-900 mb5">{intl.get('wallet.backup.not_share')}!!</div>
+          <div className="fs14 color-grey-600 mb15">{intl.get('wallet.backup.stolen')}.
           </div>
-          <div className="fs20 color-grey-900 mb5">Make a backup!!!</div>
-          <div className="fs14 color-grey-600 mb15">Secure it like the millions of dollars it may one day be worth.
+          <div className="fs20 color-grey-900 mb5">{intl.get('wallet.backup.backup')}!!!</div>
+          <div className="fs14 color-grey-600 mb15">{intl.get('wallet.backup.secure')}.
           </div>
         </div>
         <a href={blob}
            download={fileName}
-           className="ant-btn ant-btn-primary ant-btn-lg d-flex justify-content-center w-100 mt25 align-items-center">I
-          Understand, Download Wallet File</a>
+           className="ant-btn ant-btn-primary ant-btn-lg d-flex justify-content-center w-100 mt25 align-items-center">
+          {intl.get('wallet.backup.download')}</a>
       </div>
     )
   }
