65bb82a5899f495098b3d583c9444faa89256076
lyd <lyddream@126.com>
2018-03-23 16:25:24 +0800

    number locales

--- src/common/utils/localeSetting.js
+++ src/common/utils/localeSetting.js
@@ -1,34 +1,27 @@
 import intl from 'react-intl-universal';
 import locale from '../../modules/locales/localesData'
 import moment from 'moment';
-import IntlPolyfill from "intl";
 
 const locales = {
-  "en_US": locale.en_US,
-  "zh_CN": locale.zh_CN
+  "en-US": locale.en_US,
+  "zh-CN": locale.zh_CN
 };
 
 export function setLocale(value) {
 
   switch (value) {
-    case "zh_CN" :
+    case "zh-CN" :
       moment.locale('zh-cn');
       break;
-    case "en_US":
+    case "en-US":
       moment.locale('en');
       break;
     default:
       moment.locale('en');
   }
 
-  global.Intl = IntlPolyfill;
-  require('intl/locale-data/jsonp/en.js');
-  require('intl/locale-data/jsonp/zh.js');
-  require('intl/locale-data/jsonp/fr.js');
-  require('intl/locale-data/jsonp/ja.js');
-
   intl.init({
-    currentLocale: value || 'en_US',
+    currentLocale: value || 'en-US',
     locales,
   });
 
