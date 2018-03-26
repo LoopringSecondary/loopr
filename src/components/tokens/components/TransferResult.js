75961365eabff9f1359316e008522d826b30a0d4
houzongbao <houzongbao@126.com>
2018-03-26 10:35:14 +0800

    update transfer result

--- src/components/tokens/components/TransferResult.js
+++ src/components/tokens/components/TransferResult.js
@@ -9,8 +9,7 @@ let Preview = ({
   }) => {
   const {result} = modal
   const priceValue = (
-    <span className="fs10">
-      ≈
+    <span className="">
       <Currency />
       {accMul(result.extraData.amount, result.extraData.price).toFixed(2)}
     </span>
@@ -30,17 +29,17 @@ let Preview = ({
     t = intl.get('token.convert_title')
   }
   const TipsContainer = (props)=>(
-    <div className="p15 bg-grey-50 mt15" style={{borderRadius:'4px'}}>
+    <div className="p10 bg-grey-50 mt15" style={{borderRadius:'4px'}}>
       {props.children}
     </div>
   )
   const TipsTitle = (props)=>(
-    <div className="fs14 color-black-2 text-left mb5">
+    <div className="fs14 color-black-2 text-left m5">
       {props.children}
     </div>
   )
   const TipItem = (props)=>(
-    <div className="fs14 color-black-2 text-left mb5">
+    <div className="fs14 color-black-2 text-left m5">
       {props.children}
     </div>
   )
@@ -52,68 +51,52 @@ let Preview = ({
               <div className="fs14 color-grey-900">
                 <Icon className="fs60 color-red-500" type="close-circle"></Icon>
                 <div className="fs24 color-black-1 mt15">Send Failed</div>
-                <div className="fs14 color-black-2 mt10">
+                <div className="fs14 color-black-3 mt10">
                   {intl.get('token.result_failed', {do:t, amount:result.extraData.amount, token:result.extraData.tokenSymbol, reason:result.error})}
                 </div>
-                <TipsContainer>
-                  <TipsTitle>Tips</TipsTitle>
-                  <TipItem>
-                    <Icon type="exclamation-circle-o mr5 color-primary-1" />
-                    <span className="fs14 color-black-1 fs14 ">If you want to go on do this</span>
-                    <a className="fs14 ml15 color-primary-1">
-                      Go to send
-                      <Icon type="right" />
-                    </a>
-                  </TipItem>
-                  <TipItem>
-                    <Icon type="close-circle-o" className="color-red-500 mr5" />
-                    <span className="fs14 color-black-1 fs14">Some errors happened for </span>
-                    <a className="fs14 ml15 color-primary-1">
-                      Do someting
-                      <Icon type="right" />
-                    </a>
-                  </TipItem>
 
-                </TipsContainer>
               </div>
             </div>
           }
           {!result.error &&
             <div>
-              <Icon className="fs60" type="check-circle color-green-1 mb15"></Icon>
-              <div className="fs20 color-black-2 font-weight-bold">
+              <Icon className="fs60" type="check-circle color-success-1 mb15"></Icon>
+              <div className="fs20 color-black-1 font-weight-bold">
                 {t} {intl.get('token.completed')}
               </div>
               <div className="fs14 color-black-3">
                 {intl.get('token.result_success', {do:t, amount:result.extraData.amount, token:result.extraData.tokenSymbol})} ({priceValue})
               </div>
               <TipsContainer>
-                <TipsTitle>Tips</TipsTitle>
-                <TipItem>
+                <div className="fs14 color-black-2 text-left m5">
+                  {
+                    false &&
+                    <Icon type="close-circle-o" className="color-error-1 mr5" />
+                  }
                   <Icon type="exclamation-circle-o mr5 color-primary-1" />
-                  <span className="fs14 color-black-1 fs14 ">If you want to go on do this</span>
+                  <span className="fs14 color-black-1 fs14 ">Check transaction status. </span>
                   <a className="fs14 ml15 color-primary-1">
-                    Go to send
+                    View detail
                     <Icon type="right" />
                   </a>
-                </TipItem>
-                <TipItem>
-                  <Icon type="close-circle-o" className="color-red-500 mr5" />
-                  <span className="fs14 color-black-1 fs14">Some errors happened for </span>
+                </div>
+                <div className="fs14 color-black-2 text-left m5">
+                  {
+                    false &&
+                    <Icon type="close-circle-o" className="color-error-1 mr5" />
+                  }
+                  <Icon type="exclamation-circle-o mr5 color-primary-1" />
+                  <span className="fs14 color-black-1 fs14">Go on to send. </span>
                   <a className="fs14 ml15 color-primary-1">
-                    Do someting
+                    Send again
                     <Icon type="right" />
-                    {
-                      false &&
-                      <a href={`https://etherscan.io/tx/${result.extraData.txHash}`} target="_blank">{intl.get('token.view_transaction')}</a>
-                    }
                   </a>
-                </TipItem>
-
+                </div>
               </TipsContainer>
+
             </div>
           }
-          <div className="row pt20 pb20">
+          <div className="row pt20">
             <div className="col">
               {
                 result.extraData.pageFrom && result.extraData.pageFrom === 'Transfer' &&
