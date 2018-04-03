136883b1fe0524f67a0d85893344797e0df6cd2e
lyd <lyddream@126.com>
2018-04-03 17:38:14 +0800

    add

--- src/modules/socket/modules/PendingTxsContainer.js
+++ src/modules/socket/modules/PendingTxsContainer.js
@@ -1,73 +1,129 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import pricesData from '../mocks/prices.json'
+import config from '../../../common/config'
+import {toBig,toNumber} from "Loopring/common/formatter";
+
 
 class PendingTxsContainer extends React.Component {
   constructor(props, context) {
     super(props, context)
     this.state = {
-      pendingTxs:[],
+      pendingTxs: [],
     }
   }
+
   componentDidMount() {
-    const { socket } = this.context
-    if(socket){
+    const {socket} = this.context
+    if (socket) {
       const _this = this
       const owner = window.WALLET && window.WALLET.getAddress()
       const options = {
         owner
       }
-      socket.emit('pendingTxs_req',JSON.stringify(options))
-      socket.on('pendingTxs_res', (res)=>{
+      socket.emit('pendingTxs_req', JSON.stringify(options))
+      socket.on('pendingTxs_res', (res) => {
         console.log('pendingTxs_res')
         res = JSON.parse(res)
-        if(!res.error){
+        if (!res.error) {
           _this.setState({
-            pendingTxs:res.data,
+            pendingTxs: res.data,
           })
         }
       })
     }
-    if(!socket) {
+    if (!socket) {
       console.log('socket not connected')
     }
   }
+
   componentWillUnmount() {
-    const { socket } = this.context
+    const {socket} = this.context
     if (!socket) {
-      console.log('socket connection has not been established')
+      console.log('socket connection has not been established');
       return false
     }
     socket.off('pendingTxs_res')
   }
-  getTokenBySymbol(symbol,ifFormat){
-    // TODO
+
+  isOrderCanceling({validSince, tokenPair, orderHash}) {
+    const txs = this.state.pendingTxs;
+    const cancelAllTxs = txs.filter(tx => tx.type === 'cutoff');
+    let isCanceling = false;
+    cancelAllTxs.sort(function (a, b) {
+      return toNumber(b.value) - toNumber(a.value)
+    });
+    if (cancelAllTxs.length > 0 && validSince && toNumber(cancelAllTxs[0].value) > toNumber(validSince)) {
+      isCanceling = true;
+    }
+
+    if (tokenPair) {
+      const cancelMarketTx = txs.find(tx => tx.type === 'cutoff_trading_pair' && tx.content.market.toLowerCase() === tokenPair.toLowerCase());
+      isCanceling = !!cancelMarketTx
+    }
+
+    if (orderHash) {
+      const cancelOrderTx = txs.find(tx => tx.type === 'cancel_order' && tx.content.orderHash.toLowerCase() === orderHash.toLowerCase())
+      isCanceling = !!cancelOrderTx
+    }
+    return isCanceling;
   }
+
+  isApproving(symbol) {
+    if (symbol) {
+      const txs = this.state.pendingTxs;
+      const approveTxs = txs.find(tx => tx.type === 'approve' && tx.symbol.toLowerCase() === symbol.toLowerCase());
+      approveTxs.sort((a, b) => b.nonce - a.nonce);
+      if (approveTxs.length > 0) {
+        return toBig(approveTxs[0].value);
+      }
+    }
+    return -1;
+  }
+
+  isWithdrawOldWeth() {
+    const txs = this.state.pendingTxs;
+    const wethConfig = config.getTokenBySymbol('WETH_OLD');
+    const withdrawTx = txs.find(tx => tx.protocol.toLowerCase() === wethConfig.address.toLowerCase() && toBig(tx.value).eq(0));
+    return !!withdrawTx
+  }
+
+  isDepositWeth() {
+    const txs = this.state.pendingTxs;
+    const depositTxs = txs.filter(tx => tx.symbol.toLowerCase() === 'weth' && tx.type === 'convert_income');
+    let totalValue = toBig(0);
+    depositTxs.forEach(tx => totalValue = totalValue.plus(toBig(tx.value)));
+    return totalValue;
+  }
+
   render() {
-    const {children,...rest} = this.props
+    const {children, ...rest} = this.props;
     const childProps = {
       ...rest,
-      txs:{
-        items:this.state.pendingTxs,
-        getTokenBySymbol:this.getTokenBySymbol.bind(this)
+      txs: {
+        items: this.state.pendingTxs,
+        isOrderCanceling: this.isOrderCanceling.bind(this),
+        isApproving: this.isApproving.bind(this),
+        isWithdrawOldWeth: this.isWithdrawOldWeth.bind(this),
+        isDepositWeth:this.isDepositWeth.bind(this)
       }
     }
     const {render} = this.props
-    if(render){
-      return render.call(this,childProps)
+    if (render) {
+      return render.call(this, childProps)
     }
     return (
       <div>
-         {
-           React.Children.map(this.props.children, child => {
-               return React.cloneElement(child, {...childProps})
-           })
-         }
+        {
+          React.Children.map(this.props.children, child => {
+            return React.cloneElement(child, {...childProps})
+          })
+        }
       </div>
     )
   }
 }
+
 PendingTxsContainer.contextTypes = {
   socket: PropTypes.object.isRequired
 };
