0d548eb48a57f278e7f8e9319c8c9402f109bc50
houzongbao <houzongbao@126.com>
2018-04-04 15:32:55 +0800

    update alert & notification style

--- src/components/common/Alert.js
+++ src/components/common/Alert.js
@@ -1,43 +1,43 @@
 import React, { PropTypes } from 'react';
 import {Button,Icon} from 'antd';
 
-const Alert = ({title,description,type='info',theme='light',actions})=>{
+const Alert = ({title,description,type='info',theme='dark',actions,size="medium"})=>{
   return (
-    <div className={`p15 loopring-alert t-${type} t-${theme}`}>
+    <div className={`loopring-alert t-${type} t-${theme} s-${size}`}>
       <div className="row align-items-center">
         {
           type &&
-          <div className="col-auto pr0 pr15">
+          <div className="col-auto alert-gutter">
             {
               type === 'success' &&
-              <i className="icon-loopring icon-loopring-success fs35 alert-icon"  />
+              <i className="icon-loopring icon-loopring-success alert-icon"  />
               &&
-              <Icon type="check-circle-o" className="fs35 alert-icon" />
+              <Icon type="check-circle-o" className="alert-icon" />
             }
             {
               type === 'info' &&
-              <i className="icon-loopring icon-loopring-success fs35 alert-icon"  />
+              <i className="icon-loopring icon-loopring-success alert-icon"  />
               &&
-              <Icon type="info-circle-o" className="fs35 alert-icon" />
+              <Icon type="info-circle-o" className="alert-icon" />
             }
             {
               type === 'warning' &&
-              <i className="icon-loopring icon-loopring-qustion fs35 alert-icon"  />
+              <i className="icon-loopring icon-loopring-qustion alert-icon"  />
               &&
-              <Icon type="exclamation-circle-o" className="fs35 alert-icon" />
+              <Icon type="exclamation-circle-o" className="alert-icon" />
             }
 
             {
               type === 'error' &&
-              <i className="icon-loopring icon-loopring-close fs35 alert-icon"  />
+              <i className="icon-loopring icon-loopring-close alert-icon"  />
             }
           </div>
         }
-        <div className="col pl0 pr15">
-          <div className="fs18 font-weight-bold alert-title">{title}</div>
+        <div className="col alert-gutter">
+          <div className="alert-title">{title}</div>
           {
             description &&
-            <div className="fs14 mt5 alert-description" style={{lineHeight:'1.4'}}>{description}</div>
+            <div className="alert-description">{description}</div>
           }
           {
             actions &&
