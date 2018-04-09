9fef06147abdf48be93fc1e6db6fca4f0337e6ab
keith Du <keithdu@keithdeMacBook-Pro.local>
2018-04-09 10:32:38 +0800

    settings

--- src/modules/storage/settings.js
+++ src/modules/storage/settings.js
@@ -1,3 +1,4 @@
+import {configs} from '../../common/config/data'
 const set = (settings)=>{
   localStorage.settings = JSON.stringify(settings)
 }
@@ -5,10 +6,33 @@ const get = ()=>{
   if(localStorage.settings){
      return JSON.parse(localStorage.settings)
   }else{
+    const latestContract = configs.contracts[configs.contracts.length-1]
+    const relays = configs.relays
+    let sortedRelays = relays.map((item, i) => {
+      item.id = i;
+      return item
+    })
     return {
-      preference:{},
-      trading:{},
-      relay:{},
+      preference: {
+        language: "en-US",
+        currency: "USD",
+        timezone: "UTC+00:00"
+      },
+      trading: {
+        contract: {
+          version: latestContract.version,
+          address: latestContract.address
+        },
+        timeToLive: configs.defaultExpireTime,
+        timeToLiveUnit: configs.defaultExpireTimeUnit,
+        lrcFee: configs.defaultLrcFeePermillage,
+        marginSplit: configs.defaultMarginSplitPercentage,
+        gasPrice: configs.defaultGasPrice
+      },
+      relay: {
+        selected: sortedRelays[0].value,
+        nodes: sortedRelays
+      }
     }
   }
 }
