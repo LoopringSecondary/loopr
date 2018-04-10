18762ebce9f55627b5eb15f97e177f9a37583a8e
houzongbao <houzongbao@126.com>
2018-04-10 11:08:35 +0800

    update settings ui

--- src/components/settings/pages/Settings.js
+++ src/components/settings/pages/Settings.js
@@ -7,19 +7,19 @@ import intl from 'react-intl-universal';
 const SettingPage = ({modal}) => {
 
   return (
-      <Card title="Settings" className="bg-white" style={{borderRadius:'10px'}}>
-          <Tabs defaultActiveKey="preference" animated={false} tabBarStyle={{marginBottom:'0px'}}>
+      <Card title={<div className="fs1">{intl.get('navbar.settings')}</div>} className="bg-white settings-card" style={{borderRadius:'10px'}}>
+          <Tabs className="no-ink-bar setting-tabs" defaultActiveKey="preference" animated={false} tabBarStyle={{marginBottom:'0px',textAlign:'left'}}>
             <Tabs.TabPane tab={<div className="fs16 pb5 pt5">{intl.get('settings.preference')}</div>} key="preference">
               <SettingsContainer>
                 <Settings.Preference />
               </SettingsContainer>
             </Tabs.TabPane>
-            <Tabs.TabPane tab={<div className="fs18 pb5 pt5">{intl.get('settings.trade')}</div>} key="trading">
+            <Tabs.TabPane tab={<div className="fs16 pb5 pt5">{intl.get('settings.trade')}</div>} key="trading">
               <SettingsContainer>
                 <Settings.Trading />
               </SettingsContainer>
             </Tabs.TabPane>
-            <Tabs.TabPane tab={<div className="fs18 pb5 pt5">{intl.get('settings.relay')}</div>} key="relay">
+            <Tabs.TabPane tab={<div className="fs16 pb5 pt5">{intl.get('settings.relay')}</div>} key="relay">
               <SettingsContainer>
                 <Settings.Relay modal={modal} />
               </SettingsContainer>
