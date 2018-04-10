9cedab6b6bc0a483c98e1aaed832fb64d322e436
lyd <lyddream@126.com>
2018-04-10 15:43:49 +0800

    unit notification

--- src/components/tokens/components/WithdrawAll.js
+++ src/components/tokens/components/WithdrawAll.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Card, Form, Input} from 'antd';
 import {accMul} from 'Loopring/common/math'
-import {toBig, toHex, toNumber} from "Loopring/common/formatter";
+import {toBig, toHex, toNumber,getDisplaySymbol} from "Loopring/common/formatter";
 import Currency from '../../../modules/settings/CurrencyContainer'
 import wrapArrow from '../../../assets/images/wrap-arrow.png';
 import {generateAbiData} from '../../../common/Loopring/ethereum/abi'
@@ -9,6 +9,9 @@ import config from '../../../common/config'
 import {notifyTransactionSubmitted} from 'Loopring/relay/utils'
 import intl from 'react-intl-universal';
 import CoinIcon from '../../common/CoinIcon';
+import Notification from 'Loopr/Notification'
+import * as math from '../../../common/Loopring/common/math'
+
 
 class WithdrawAll extends React.Component {
 
@@ -79,7 +82,9 @@ class WithdrawAll extends React.Component {
         </Form>
       </Card>
     );
-
+    function viewInEtherscan (txHash){
+      window.open(`https://etherscan.io/tx/${txHash}`,'_blank')
+    }
 
     function handleSubmit() {
       const _this = this;
@@ -96,8 +101,21 @@ class WithdrawAll extends React.Component {
               window.STORAGE.wallet.setWallet({address:window.WALLET.getAddress(),nonce:nonce});
               notifyTransactionSubmitted(res.result);
               modal.hideModal({id:'token/withdrawall'});
-              const result = {extraData:{txHash:res.result, amount:values.amount, price:price.price, tokenSymbol:selectedToken.symbol, pageFrom:'WithdrawAll'}};
-              modal.showModal({id:'token/transfer/result', result})
+              // const result = {extraData:{txHash:res.result, amount:values.amount, price:price.price, tokenSymbol:selectedToken.symbol, pageFrom:'WithdrawAll'}};
+              // modal.showModal({id:'token/transfer/result', result});
+
+              const worth = `${getDisplaySymbol(window.STORAGE.settings.get().preference.currency)}${math.accMul(values.amount, price.price).toFixed(2)}`;
+              Notification.open({
+                message:`${intl.get('token.convert_title')} ${intl.get('token.completed')}`,
+                description:`${intl.get('token.result_success', {do:intl.get('token.convert_title'), amount:values.amount, token:selectedToken.symbol})} (${worth})`,
+                type:'success',
+                actions:(
+                  <div>
+                    <Button className="alert-btn mr5" onClick={this.viewInEtherscan.bind(this, res.result)}>{intl.get('token.transfer_result_etherscan')}</Button>
+                  </div>
+                )
+              })
+
             }
           }).catch(error=>{
             console.error(error)
