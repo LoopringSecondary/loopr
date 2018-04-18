2abe352a7279d5254f90b4677c9bb1a35cda023b
lyd <lyddream@126.com>
2018-04-18 18:44:30 +0800

    check input

--- src/common/Loopring/ethereum/contracts/AbiFunction.js
+++ src/common/Loopring/ethereum/contracts/AbiFunction.js
@@ -1,30 +1,32 @@
-
 import {rawEncode, methodID} from 'ethereumjs-abi';
-
+import {toHex} from "../../common/formatter";
+import every from 'lodash/every'
 
 export default class AbiFunction {
 
-  constructor({inputs,name,outputs,constant}){
+  constructor({inputs, name, outputs, constant}) {
     this.name = name;
     this.inputTypes = inputs.map(({type}) => type);
-    this.inputNames = inputs.map(({name}) => name);
+    this.inputs = inputs;
     this.outputTypes = outputs.map(({type}) => type);
-    this.outputNames =outputs.map(({name})=> name);
+    this.outputs = outputs;
     this.constant = constant;
-    this.methodAbiHash = methodID(name,this.inputTypes);
+    this.methodAbiHash = toHex(methodID(name, this.inputTypes));
   }
 
 
-  encodeInputs(inputs){
-    const abiInputs = this
+  encodeInputs(inputs) {
+    this.checkInputs(inputs);
 
 
   }
 
-  checkInputs(inputs){
-
-
-
+  checkInputs(inputs) {
+    this.inputs.forEach(({name, type}) => {
+      if (!inputs[name]) {
+        throw new Error(`Parameter ${name} of type ${type} is required!`)
+      }
+    })
   }
 
 }
