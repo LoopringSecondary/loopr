bb9eb522a68380c9a12c2979fb86634302fff635
lyd <lyddream@126.com>
2018-04-20 11:27:06 +0800

    fix  ￥

--- src/modules/settings/CurrencyContainer.js
+++ src/modules/settings/CurrencyContainer.js
@@ -19,7 +19,7 @@ class CurrencyContainer extends React.Component {
         icon:'￥',
         title:'Yuan',
       };
-      return '￥'
+      return <span style={{marginRight:"-2px"}}>￥</span>
     }
   }
 }
