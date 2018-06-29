b590317bd1c6cf46ddcec666a516e10c44f6e1ef
lyd <lyddream@126.com>
2018-06-29 15:16:53 +0800

    disable claim LRN and fix grammer

--- src/modules/locales/localesData/en_US.js
+++ src/modules/locales/localesData/en_US.js
@@ -44,7 +44,7 @@ export default {
   version:{
     label:'Beta3',
     title:'Beta3',
-    description:'LOOPRING.IO (Loopr2) is an open-sourced reference implementeation of a web-based non-custodial Ethereum wallet that enable users to trade ERC20 tokens using the Loopring Protocol. Before proceed, you should review the codebase. Loopr2 is not for for-profit product and the Loopring Foundation will not take responsiblity for any loss of your crypto-assets.',
+    description:'LOOPRING.IO (Loopr2) is an open-sourced implementation of a web-based non-custodial Ethereum wallet that enables users to trade ERC20 tokens using the Loopring Protocol. Before proceeding, you should review the codebase. Loopr2 is a not-for-profit product, and the Loopring Foundation cannot take responsibility for any loss of your crypto-assets.',
     feedback:'Feedback',
     feedback_submit:'Report an issue',
     roadmap_label:'More Info',
